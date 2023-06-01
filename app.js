94195c6ee026bb2994913c6279735c8c83cb5a95
Alan Recupero <alan.recupero@gmail.com>
2023-06-01 20:24:51 -0300

    Atualizando as rotas

--- app.js
+++ app.js
@@ -4,74 +4,86 @@ const app = express();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'cloud_atv4'
+  host: 'localhost',
+  user: 'root',
+  database: 'cloud_atv4'
 });
 
 app.get("/", (request, response) => {
-    return response
-        .status(200)
-        .json({
-            message: "Teste"
-        });
+  return response
+    .status(200)
+    .json({
+      message: "Teste"
+    });
 });
 
 app.get("/liveness", (request, response) => {
-    return response
-        .status(200)
-        .json({
-            message: "APP Ativo",
-            date: new Date().getTime()
-        });
+  return response
+    .status(200)
+    .json({
+      message: "APP Ativo",
+      date: new Date().getTime()
+    });
 });
 
 app.get("/readiness", (request, response) => {
-    return response
-        .status(200)
-        .json({
-            message: "APP Pronto",
-            date: new Date().getTime()
-        });
+  return response
+    .status(200)
+    .json({
+      message: "APP Pronto",
+      date: new Date().getTime()
+    });
 });
 
 app.get("/select", (request, response) => {
-    return response
-        .status(200)
-        .json({
-            message: "Selecionando dados",
-            date: new Date().getTime()
-        });
+  return response
+    .status(200)
+    .json({
+      message: "Selecionando dados",
+      date: new Date().getTime()
+    });
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Conectado ao banco de dados");
-    const sql = "CREATE TABLE ativ04 (Chave INT AUTO_INCREMENT PRIMARY KEY, campo01 VARCHAR(50), campo02 VARCHAR(50), campo03 VARCHAR(50))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result)
-      console.log("Tabela ativ04 Criada");
+app.get('/criar-tabela', function (request, response) {
+  connection.query(
+    `CREATE TABLE ativ04 (
+    chave INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    campo01 VARCHAR(50) NOT NULL,
+    campo02 VARCHAR(50),
+    campo03 VARCHAR(50));`,
+    function (err, result) {
+      if (err) {
+        return response.status(500).json('Erro ao criar tabela');
+      }
+      else {
+        return response.json(result);
+      }
     });
-  });
+});
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Conectado ao banco de dados");
-    const sql = "INSERT INTO ativ04 (campo01, campo02, campo03) VALUES ('valor1', 'valor2', 'valor3')";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log(result)
-      console.log("1 registro inserido");
+app.get('/inserir-dados', function (request, response) {
+  connection.query(
+    `INSERT INTO ativ04 (campo01, campo02, campo03) VALUES ('valor1', 'valor2', 'valor3');`,
+    function (err, result) {
+      if (err) {
+        return response.status(500).json('Erro ao inserir dados na tabela');
+      }
+      else {
+        return response.json(result);
+      }
     });
-  });
+});
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM ativ04", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
+app.get('/consulta-dados', function (request, response) {
+  connection.query("SELECT * FROM ativ04;",
+    function (err, result) {
+      if (err) {
+        return response.status(500).json('Erro ao consultar os dados.');
+      }
+      else {
+        return response.json(result);
+      }
     });
-  });
+});
 
 module.exports = app;
